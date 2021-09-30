736599ab5783b2db843f38a341189c6ec4010dcc
VasuaLun <v.a.parhaev@gmail.com>
2021-09-30 17:15:59 +0300

    Прописанные задачи для Itemов, задел для работы кнопки save

--- Page 794.sql
+++ Page 794.sql
@@ -1,4 +1,4 @@
-
+-- Вывод данных в itemы
 declare default_value varchar2(24);
 begin
     select D.NOTES into default_value
@@ -31,3 +31,26 @@ end;
 Select NEXT_PERIOD, PLAN1, PLAN2
   into nNEXTPERIOD, nPLAN1, nPLAN2
   from Z_VERSIONS where RN = pVersion;
+
+declare outcome number;
+begin
+    outcome := nvl(:P794_P794_1PERCOUNT, 0) * nvl(:P794_1PAY, 0) * nvl(:P794_1DAYSCOUNT, 0);
+    return outcome;
+end;
+
+declare outcome number;
+begin
+    outcome := nvl(:P794_2PERCOUNT, 0) * nvl(:P794_2PAY, 0) * nvl(:P794_2DAYSCOUNT, 0);
+    return outcome;
+end;
+
+-- Работа кнопки SAVE
+
+
+-- Запрос для item P794_ACODE, для выбора целевой статьи
+select F.CODE, F.NAME
+from Z_FUNDS F, Z_FUNDS_ORGS FO
+where F.JUR_PERS = 1351099
+    and F.VERSION = 344076908
+    and FO.FUND_RN = F.RN
+    and FO.ORG_RN = 344150893
