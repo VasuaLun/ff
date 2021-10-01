0ffc181e73aff68cd46acc401091ae716edc3f2a
VasuaLun <v.a.parhaev@gmail.com>
2021-10-01 10:50:39 +0300

    Редактирование страниц 793 и 794

--- Page 794.sql
+++ Page 794.sql
@@ -45,9 +45,41 @@ begin
 end;
 
 -- Работа кнопки SAVE
+declare
+    str_rn long := :P2011_RN_ARR;
+    rn_depfin number := :P2011_DEPFIN_RN;
+    rn_jurpers number;
+    rn_array dbms_utility.lname_array;
+    value_array dbms_utility.lname_array;
+    rn_count binary_integer;
+    value_count binary_integer;
 
+begin
+    if str_rn = 'null' or  str_rn is null or trim(str_rn) = '' then
+        ZP_EXCEPTION (0, 'Не выбрано ни одной записи');
+
+    else
+            rn_jurpers := to_number(substr(rn_array(i),2));
+            INSERT INTO Z_DEPFIN_GRBS(DEPFIN_RN, JURPERS_RN) VALUES (rn_depfin, rn_jurpers);
+        :P2011_RN_ARR := null;
+    end if;
+end;
 
 -- Запрос для item P794_ACODE, для выбора целевой статьи
+select F.CODE, F.NAME
+from Z_FUNDS F, Z_FUNDS_ORGS FO
+where F.JUR_PERS = 1351099
+    and F.VERSION = 344076908
+    and FO.FUND_RN = F.RN
+    and FO.ORG_RN =  nvl(:P1_ORGRN,:P7_ORGFILTER)
+
+select F.CODE, F.NAME
+from Z_FUNDS F, Z_FUNDS_ORGS FO
+where F.JUR_PERS = :P1_JURPERS
+    and F.VERSION = :P1_VERSION
+    and FO.FUND_RN = F.RN
+    and FO.ORG_RN =  nvl(:P1_ORGRN,:P7_ORGFILTER)
+
 select F.CODE, F.NAME
 from Z_FUNDS F, Z_FUNDS_ORGS FO
 where F.JUR_PERS = 1351099
