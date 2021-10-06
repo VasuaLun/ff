fed06ed5e98c324988a8fab1bf17bc35cdae348d
VasuaLun <v.a.parhaev@gmail.com>
2021-10-06 12:54:40 +0300

    -

--- Наработки для сообщений.sql
+++ Наработки для сообщений.sql
@@ -1,2 +1,5 @@
 -- Выборка аналитиков из z_users
 select * from Z_USERS where upper(POSITION) like 'АНАЛИТИК%'
+
+-- Распределение сообщений по ГРБС
+select * from V_MCTICKET_BASE where NJURPERS = 1508934 order by DDATE_CREATE desc SJURPERS  desc
